86638c666595066fe1e13cc7db6525ed6c481f6d
Herbert Poul <herbert@poul.at>
2019-09-12 10:26:30 +0200

    fix source distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,13 @@ if (secretConfig.exists()) {
 }
 
 tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.main.get().allJava)
+    from(sourceSets.main.get().allSource)
     archiveClassifier.set("sources")
 }
+//val sourcesJar by tasks.registering(Jar::class) {
+//    archiveClassifier.set("sources")
+//    from(sourceSets.main.get().allSource)
+//}
 tasks.register<Jar>("javadocJar") {
     from(tasks.javadoc)
     archiveClassifier.set("javadoc")
@@ -34,6 +38,7 @@ publishing {
         create<MavenPublication>("mavenCentralJava") {
             from(components["java"])
             artifact(tasks["sourcesJar"])
+//            artifact(sourcesJar.get())
             artifact(tasks["javadocJar"])
 
 
